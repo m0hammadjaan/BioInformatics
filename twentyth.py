7b7eabf5893620ccbdb8c20c5438e045e7f10d43
m0hammadjaan <janmuhammad2050@gmail.com>
2023-01-07 14:48:19 +0500

    Kmers Composition

--- twentyth.py
+++ twentyth.py
@@ -1,6 +1,6 @@
 def kmerComposition(nucleotideSeq, k):
     composition = {}
-    for i in range(len(nucleotideSeq) - k - 1):
+    for i in range(len(nucleotideSeq) - (k - 1)):
         kmer = nucleotideSeq[i:i+k]
         if kmer in composition:
             composition[kmer]+= 1
@@ -14,11 +14,16 @@ def kmerComposition(nucleotideSeq, k):
 
 
 if __name__ == '__main__':
-    
-    composition = kmerComposition(s, 4)
+    with open('rosalind_kmer.txt') as f:
+        data = f.read()
+        seq = data.split('>Rosalind_')
+        dna = seq[1][4:].replace('\n','')
+    composition = kmerComposition(dna, 4)
     residue = list(composition.keys())
     residue.sort()
-    print(residue)
+
 
     for i in range(len(residue)):
         print(composition[residue[i]], end=' ')
+
+    
